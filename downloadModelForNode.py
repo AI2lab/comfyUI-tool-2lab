fadf9ae74944229a6e93b2ad3f1be3c18536d9c9
liguo <bakkhos@gmail.com>
2024-06-30 01:32:10 +0800

    downloadModelForNode.py

---
+++ downloadModelForNode.py
@@ -0,0 +1,19 @@
+import argparse
+
+from nodes.utils import  auto_download_model
+
+def main(node):
+    print(f"Node: {node}")
+    auto_download_model(node)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="download models for custom node")
+
+    # 添加一个可选参数 --node
+    parser.add_argument('--node', type=str, default='', help='Specify the node folder name')
+
+    # 解析命令行参数
+    args = parser.parse_args()
+
+    # 调用主函数并传递解析后的参数
+    main(args.node)
---
+++ nodes/local/localWorker.py
@@ -0,0 +1,4 @@
+
+from ..constants import get_project_category
+
+NODE_CATEGORY = get_project_category("local")
